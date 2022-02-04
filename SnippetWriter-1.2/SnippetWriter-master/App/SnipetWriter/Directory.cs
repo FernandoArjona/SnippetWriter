7e1f824b222d7019e09e72fed8690ece7c65c8fa
FernandoArjona <62151689+FernandoArjona@users.noreply.github.com>
2022-02-04 12:49:30 -0600

    Cambio de presentacion y organización de folders

---
+++ SnippetWriter-1.2/SnippetWriter-master/App/SnipetWriter/Directory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnipetWriter
+{
+    class Directory
+    {
+        public string lastAdress { get; set; }
+        public List<string> projectPaths { get; set; }
+        public int writerIndex { get; set; }
+        public Directory ()
+        {
+            this.projectPaths = new List<string>();
+        }
+    }
+}
